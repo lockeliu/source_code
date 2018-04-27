2e7c988456c6b18826be906c150e6a822ee15021
lockeliu <lockeliu@tencent.com>
2018-04-27 19:55:36 +0800

    up

--- caffe/include/caffe/layers/lifted_struct_similarity_softmax_layer.hpp
+++ caffe/include/caffe/layers/lifted_struct_similarity_softmax_layer.hpp
@@ -11,36 +11,36 @@
 
 namespace caffe {
 
-template <typename Dtype>
-class LiftedStructSimilaritySoftmaxLossLayer : public LossLayer<Dtype> {
- public:
-  explicit LiftedStructSimilaritySoftmaxLossLayer(const LayerParameter& param)
-      : LossLayer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+        template <typename Dtype>
+                class LiftedStructSimilaritySoftmaxLossLayer : public LossLayer<Dtype> {
+                        public:
+                                explicit LiftedStructSimilaritySoftmaxLossLayer(const LayerParameter& param)
+                                        : LossLayer<Dtype>(param) {}
+                                virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+                                                const vector<Blob<Dtype>*>& top);
 
-  virtual inline int ExactNumBottomBlobs() const { return 2; }
-  virtual inline const char* type() const { return "LiftedStructSimilaritySoftmaxLoss"; }
-  virtual inline bool AllowForceBackward(const int bottom_index) const {
-    return bottom_index != 1;
-  }
+                                virtual inline int ExactNumBottomBlobs() const { return 2; }
+                                virtual inline const char* type() const { return "LiftedStructSimilaritySoftmaxLoss"; }
+                                virtual inline bool AllowForceBackward(const int bottom_index) const {
+                                        return bottom_index != 1;
+                                }
 
- protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+                        protected:
+                                virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+                                                const vector<Blob<Dtype>*>& top);
 
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+                                virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+                                                const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
-  Blob<Dtype> dist_sq_;  // cached for backward pass
-  Blob<Dtype> dot_;
-  Blob<Dtype> ones_;
-  Blob<Dtype> blob_pos_diff_;
-  Blob<Dtype> blob_neg_diff_;
-  Blob<Dtype> loss_aug_inference_;
-  Blob<Dtype> summer_vec_;
-  Dtype num_constraints;
-};
+                                Blob<Dtype> dist_sq_;  // cached for backward pass
+                                Blob<Dtype> dot_;
+                                Blob<Dtype> ones_;
+                                Blob<Dtype> blob_pos_diff_;
+                                Blob<Dtype> blob_neg_diff_;
+                                Blob<Dtype> loss_aug_inference_;
+                                Blob<Dtype> summer_vec_;
+                                Dtype num_constraints;
+                };
 
 }  // namespace caffe
 
